054770f14b57fff4081ff75781a309cdbcc7a057
Robert Crooks <rcrooks@brightcove.com>
2020-05-09 11:31:28 -0400

    rebuild

---
+++ _site/pull-all.sh
@@ -0,0 +1,74 @@
+git pull
+
+echo 'pulled general content'
+# apis
+cd ../apis
+git pull
+
+echo 'pulled apis'
+# audience
+cd ../audience
+git pull
+
+echo 'pulled audience'
+# beacon
+cd ../beacon-support
+git pull
+
+echo 'pulled beacon'
+# campaign
+git pull
+cd ../campaign
+
+echo 'pulled campaign'
+# docs-cms-connectors
+cd ../docs-cms-connectors
+git pull
+
+echo 'pulled docs-cms-connectors'
+# engage
+cd ../engage
+git pull
+
+echo 'pulled engage'
+# gallery
+cd ../gallery
+git pull
+
+echo 'pulled engage'
+# live
+cd ../live
+git pull
+
+echo 'pulled live'
+# player-dev
+cd ../player-dev
+git pull
+
+echo 'pulled player-dev'
+# sdks
+cd ../sdks
+git pull
+
+echo 'pulled sdks'
+# social
+cd ../social
+git pull
+
+echo 'pulled social'
+# ssai
+cd ../ssai
+git pull
+
+echo 'pulled ssai'
+# studio
+cd ../studio
+git pull
+
+echo 'pulled studio'
+# zencoder-support
+cd ../zencoder-support
+git pull
+
+echo 'pulled zencoder-support'
+echo 'all finished!'
