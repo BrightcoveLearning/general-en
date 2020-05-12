137f070774662427611e34e183b741e696ae695a
Robert Crooks <rcrooks@brightcove.com>
2020-05-12 07:55:33 -0400

    rebuild, add serve-all script

---
+++ _site/serve-all.sh
@@ -0,0 +1,119 @@
+#
+bundle update
+jekyll serve
+#
+#
+echo 'serving general content'
+# apis
+cd ../apis
+#
+bundle update
+jekyll serve
+#
+#
+echo 'serving apis'
+# audience
+cd ../audience
+#
+bundle update
+jekyll serve
+#
+#
+echo 'serving audience'
+# beacon
+cd ../beacon-support
+#
+bundle update
+jekyll serve
+#
+#
+echo 'serving beacon'
+# campaign
+#
+cd ../campaign
+bundle update
+jekyll serve
+#
+#
+echo 'serving campaign'
+# docs-cms-connectors
+cd ../docs-cms-connectors
+#
+bundle update
+jekyll serve
+#
+#
+echo 'serving docs-cms-connectors'
+# engage
+cd ../engage
+#
+bundle update
+jekyll serve
+#
+#
+echo 'serving engage'
+# gallery
+cd ../gallery
+#
+bundle update
+jekyll serve
+#
+#
+echo 'serving engage'
+# live
+cd ../live
+#
+bundle update
+jekyll serve
+#
+#
+echo 'serving live'
+# player-dev
+cd ../player-dev
+#
+bundle update
+jekyll serve
+#
+#
+echo 'serving player-dev'
+# sdks
+cd ../sdks
+#
+bundle update
+jekyll serve
+#
+#
+echo 'serving sdks'
+# social
+cd ../social
+#
+bundle update
+jekyll serve
+#
+#
+echo 'serving social'
+# ssai
+cd ../ssai
+#
+bundle update
+jekyll serve
+#
+#
+echo 'serving ssai'
+# studio
+cd ../studio
+#
+bundle update
+jekyll serve
+#
+#
+echo 'serving studio'
+# zencoder-support
+cd ../zencoder-support
+#
+bundle update
+jekyll serve
+#
+#
+echo 'serving zencoder-support'
+echo 'all finished!'
