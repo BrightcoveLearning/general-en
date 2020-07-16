bc572ae70704db0e8aa20384b1fcc157f731fecd
Robert Crooks <rcrooks@brightcove.com>
2020-07-16 12:12:47 -0400

    update script

--- bundle-update.sh
+++ bundle-update.sh
@@ -1,94 +1,113 @@
 git pull
+bundle install
 sudo bundle update
 echo 'bundle updated general content'
 # apis
 cd ../apis
 git pull
+bundle install
 sudo bundle update
 echo 'bundle updated apis'
 # audience
 cd ../audience
 git pull
+bundle install
 sudo bundle update
 echo 'bundle updated audience'
 # beacon
 cd ../beacon-support
 git pull
+bundle install
 sudo bundle update
 echo 'bundle updated beacon'
 # campaign
 git pull
 cd ../campaign
+bundle install
 sudo bundle update
 echo 'bundle updated campaign'
 # docs-cms-connectors
 cd ../docs-cms-connectors
 git pull
+bundle install
 sudo bundle update
 echo 'bundle updated docs-cms-connectors'
 # docs-support-home
 cd ../docs-support-home
 git pull
+bundle install
 sudo bundle update
 echo 'bundle updated docs-support-home'
 # engage
 cd ../engage
 git pull
+bundle install
 sudo bundle update
 echo 'bundle updated engage'
 # gallery
 cd ../gallery
 git pull
+bundle install
 sudo bundle update
 echo 'bundle updated gallery'
 # jekyll-template
 cd ../jekyll-template
 git pull
+bundle install
 sudo bundle update
 echo 'bundle updated jekyll-template'
 # live
 cd ../live
 git pull
+bundle install
 sudo bundle update
 echo 'bundle updated live'
 # player-dev
 cd ../player-dev
 git pull
+bundle install
 sudo bundle update
 echo 'bundle updated player-dev'
 # sdks
 cd ../sdks
 git pull
+bundle install
 sudo bundle update
 echo 'bundle updated sdks'
 # social
 cd ../social
 git pull
+bundle install
 sudo bundle update
 echo 'bundle updated social'
 # ssai
 cd ../ssai
 git pull
+bundle install
 sudo bundle update
 echo 'bundle updated ssai'
 # studio
 cd ../studio
 git pull
+bundle install
 sudo bundle update
 echo 'bundle updated studio'
 # zencoder-support
 cd ../zencoder-support
 git pull
+bundle install
 sudo bundle update
 echo 'bundle updated zencoder-support'
 # test site
 cd ../test-site
 git pull
+bundle install
 sudo bundle update
 echo 'bundle updated test-site'
 # stagine site
 cd ../staging-site
 git pull
+bundle install
 sudo bundle update
 echo 'bundle updated staging-site'
 echo 'all finished!'
