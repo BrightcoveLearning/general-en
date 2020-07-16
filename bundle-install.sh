22dea59c1a76b630ec6adeb48b986be0cbc5d5b2
Robert Crooks <rcrooks@brightcove.com>
2020-07-16 11:22:50 -0400

    Create bundle-install.sh

---
+++ bundle-install.sh
@@ -0,0 +1,94 @@
+git pull
+sudo bundle install
+echo 'bundle installed general content'
+# apis
+cd ../apis
+git pull
+sudo bundle install
+echo 'bundle installed apis'
+# audience
+cd ../audience
+git pull
+sudo bundle install
+echo 'bundle installed audience'
+# beacon
+cd ../beacon-support
+git pull
+sudo bundle install
+echo 'bundle installed beacon'
+# campaign
+git pull
+cd ../campaign
+sudo bundle install
+echo 'bundle installed campaign'
+# docs-cms-connectors
+cd ../docs-cms-connectors
+git pull
+sudo bundle install
+echo 'bundle installed docs-cms-connectors'
+# docs-support-home
+cd ../docs-support-home
+git pull
+sudo bundle install
+echo 'bundle installed docs-support-home'
+# engage
+cd ../engage
+git pull
+sudo bundle install
+echo 'bundle installed engage'
+# gallery
+cd ../gallery
+git pull
+sudo bundle install
+echo 'bundle installed gallery'
+# jekyll-template
+cd ../jekyll-template
+git pull
+sudo bundle install
+echo 'bundle installed jekyll-template'
+# live
+cd ../live
+git pull
+sudo bundle install
+echo 'bundle installed live'
+# player-dev
+cd ../player-dev
+git pull
+sudo bundle install
+echo 'bundle installed player-dev'
+# sdks
+cd ../sdks
+git pull
+sudo bundle install
+echo 'bundle installed sdks'
+# social
+cd ../social
+git pull
+sudo bundle install
+echo 'bundle installed social'
+# ssai
+cd ../ssai
+git pull
+sudo bundle install
+echo 'bundle installed ssai'
+# studio
+cd ../studio
+git pull
+sudo bundle install
+echo 'bundle installed studio'
+# zencoder-support
+cd ../zencoder-support
+git pull
+sudo bundle install
+echo 'bundle installed zencoder-support'
+# test site
+cd ../test-site
+git pull
+sudo bundle install
+echo 'bundle installed test-site'
+# stagine site
+cd ../staging-site
+git pull
+sudo bundle install
+echo 'bundle installed staging-site'
+echo 'all finished!'
