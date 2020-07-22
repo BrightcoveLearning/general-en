994f26f708afffdd5b8e4b87ecc76d52bb815a49
Robert Crooks <rcrooks@brightcove.com>
2020-07-22 18:10:23 -0400

    general-update

---
+++ _site/push-all.sh
@@ -0,0 +1,104 @@
+git push
+
+echo 'pushed general content'
+# apis
+cd ../apis
+git push
+
+echo 'pushed apis'
+# audience
+cd ../audience
+git push
+
+echo 'pushed audience'
+# beacon
+cd ../beacon-support
+git push
+
+echo 'pushed beacon'
+# campaign
+git push
+cd ../campaign
+
+echo 'pushed campaign'
+# docs-cms-connectors
+cd ../docs-cms-connectors
+git push
+
+echo 'pushed docs-cms-connectors'
+# docs-support-home
+cd ../docs-support-home
+git push
+
+echo 'pushed docs-support-home'
+# engage
+cd ../engage
+git push
+
+echo 'pushed engage'
+# gallery
+cd ../gallery
+git push
+
+echo 'pushed engage'
+# jekyll-template
+cd ../jekyll-template
+git push
+
+echo 'pushed jekyll-template'
+# live
+cd ../live
+git push
+
+echo 'pushed live'
+# player-dev
+cd ../player-dev
+git push
+
+echo 'pushed player-dev'
+# sdks
+cd ../sdks
+git push
+
+echo 'pushed sdks'
+# social
+cd ../social
+git push
+
+echo 'pushed social'
+# ssai
+cd ../ssai
+git push
+
+echo 'pushed ssai'
+# studio
+cd ../studio
+git push
+
+echo 'pushed studio'
+# zencoder-support
+cd ../zencoder-support
+git push
+
+# staging-site
+cd ../staging-site
+git push
+echo 'pushed staging site'
+
+# test-site
+cd ../test-site
+git push
+echo 'pushed test site'
+
+# BCL-developer-docs
+cd ../BCL-developer-docs
+git push
+echo 'pushed zencoder-support'
+
+echo 'pushed BCL-developer-docs'
+# BCL-Doc-Assets
+cd ../BCL-Doc-Assets
+git push
+
+echo 'pushed BCL-Doc-Assets'
+echo 'all finished!'
