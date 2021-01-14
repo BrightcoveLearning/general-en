60fe3a63009801e8a96eb8cf29ed87eac4bb8f0d
Jeff Doktor <jdoktor@brightcove.com>
2021-01-14 09:15:37 -0700

    add china delivery to staging

---
+++ _site/update-loc.sh
@@ -0,0 +1,595 @@
+sudo chmod -R g+w /Users/rcrooks/github
+# apis
+cd /Users/rcrooks/git-loc/apis-de
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated apis-de'
+cd /Users/rcrooks/git-loc/apis-es
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated apis-es'
+cd /Users/rcrooks/git-loc/apis-fr
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated apis-fr'
+cd /Users/rcrooks/git-loc/apis-ja
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated apis-ja'
+cd /Users/rcrooks/git-loc/apis-ko
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated apis-ko'
+cd /Users/rcrooks/git-loc/apis-zh-tw
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated apis-zh-tw'
+# audience
+cd /Users/rcrooks/git-loc/audience-de
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated audience-de'
+cd /Users/rcrooks/git-loc/audience-es
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated audience-es'
+cd /Users/rcrooks/git-loc/audience-fr
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated audience-fr'
+cd /Users/rcrooks/git-loc/audience-ja
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated audience-ja'
+cd /Users/rcrooks/git-loc/audience-ko
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated audience-ko'
+cd /Users/rcrooks/git-loc/audience-zh-tw
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated audience-zh-tw'
+# beacon
+cd /Users/rcrooks/git-loc/beacon-de
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated beacon-de'
+cd /Users/rcrooks/git-loc/beacon-es
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated beacon-es'
+cd /Users/rcrooks/git-loc/beacon-fr
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated beacon-fr'
+cd /Users/rcrooks/git-loc/beacon-ja
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated beacon-ja'
+cd /Users/rcrooks/git-loc/beacon-ko
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated beacon-ko'
+cd /Users/rcrooks/git-loc/beacon-zh-tw
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated beacon-zh-tw'
+# campaign
+cd /Users/rcrooks/git-loc/campaign-de
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated campaign-de'
+cd /Users/rcrooks/git-loc/campaign-es
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated campaign-es'
+cd /Users/rcrooks/git-loc/campaign-fr
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated campaign-fr'
+cd /Users/rcrooks/git-loc/campaign-ja
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated campaign-ja'
+cd /Users/rcrooks/git-loc/campaign-ko
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated campaign-ko'
+cd /Users/rcrooks/git-loc/campaign-zh-tw
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated campaign-zh-tw'
+# docs-support-home
+cd /Users/rcrooks/git-loc/docs-support-home-de
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated docs-support-home-de'
+cd /Users/rcrooks/git-loc/docs-support-home-es
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated docs-support-home-es'
+cd /Users/rcrooks/git-loc/docs-support-home-fr
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated docs-support-home-fr'
+cd /Users/rcrooks/git-loc/docs-support-home-ja
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated docs-support-home-ja'
+cd /Users/rcrooks/git-loc/docs-support-home-ko
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated docs-support-home-ko'
+cd /Users/rcrooks/git-loc/docs-support-home-zh-tw
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated docs-support-home-zh-tw'
+# engage
+cd /Users/rcrooks/git-loc/engage-de
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated engage-de'
+cd /Users/rcrooks/git-loc/engage-es
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated engage-es'
+cd /Users/rcrooks/git-loc/engage-fr
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated engage-fr'
+cd /Users/rcrooks/git-loc/engage-ja
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated engage-ja'
+cd /Users/rcrooks/git-loc/engage-ko
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated engage-ko'
+cd /Users/rcrooks/git-loc/engage-zh-tw
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated engage-zh-tw'
+# gallery
+cd /Users/rcrooks/git-loc/gallery-de
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated gallery-de'
+cd /Users/rcrooks/git-loc/gallery-es
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated gallery-es'
+cd /Users/rcrooks/git-loc/gallery-fr
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated gallery-fr'
+cd /Users/rcrooks/git-loc/gallery-ja
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated gallery-ja'
+cd /Users/rcrooks/git-loc/gallery-ko
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated gallery-ko'
+cd /Users/rcrooks/git-loc/gallery-zh-tw
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated gallery-zh-tw'
+# general
+cd /Users/rcrooks/git-loc/general-de
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated general-de'
+cd /Users/rcrooks/git-loc/general-es
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated general-es'
+cd /Users/rcrooks/git-loc/general-fr
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated general-fr'
+cd /Users/rcrooks/git-loc/general-ja
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated general-ja'
+cd /Users/rcrooks/git-loc/general-ko
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated general-ko'
+cd /Users/rcrooks/git-loc/general-zh-tw
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated general-zh-tw'
+# integrations
+cd /Users/rcrooks/git-loc/integrations-de
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated integrations-de'
+cd /Users/rcrooks/git-loc/integrations-es
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated integrations-es'
+cd /Users/rcrooks/git-loc/integrations-fr
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated integrations-fr'
+cd /Users/rcrooks/git-loc/integrations-ja
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated integrations-ja'
+cd /Users/rcrooks/git-loc/integrations-ko
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated integrations-ko'
+cd /Users/rcrooks/git-loc/integrations-zh-tw
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated integrations-zh-tw'
+# live
+cd /Users/rcrooks/git-loc/live-de
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated live-de'
+cd /Users/rcrooks/git-loc/live-es
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated live-es'
+cd /Users/rcrooks/git-loc/live-fr
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated live-fr'
+cd /Users/rcrooks/git-loc/live-ja
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated live-ja'
+cd /Users/rcrooks/git-loc/live-ko
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated live-ko'
+cd /Users/rcrooks/git-loc/live-zh-tw
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated live-zh-tw'
+# player
+cd /Users/rcrooks/git-loc/player-de
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated player-de'
+cd /Users/rcrooks/git-loc/player-es
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated player-es'
+cd /Users/rcrooks/git-loc/player-fr
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated player-fr'
+cd /Users/rcrooks/git-loc/player-ja
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated player-ja'
+cd /Users/rcrooks/git-loc/player-ko
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated player-ko'
+cd /Users/rcrooks/git-loc/player-zh-tw
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated player-zh-tw'
+# sdks
+cd /Users/rcrooks/git-loc/sdks-de
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated sdks-de'
+cd /Users/rcrooks/git-loc/sdks-es
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated sdks-es'
+cd /Users/rcrooks/git-loc/sdks-fr
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated sdks-fr'
+cd /Users/rcrooks/git-loc/sdks-ja
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated sdks-ja'
+cd /Users/rcrooks/git-loc/sdks-ko
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated sdks-ko'
+cd /Users/rcrooks/git-loc/sdks-zh-tw
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated sdks-zh-tw'
+# social
+cd /Users/rcrooks/git-loc/social-de
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated social-de'
+cd /Users/rcrooks/git-loc/social-es
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated social-es'
+cd /Users/rcrooks/git-loc/social-fr
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated social-fr'
+cd /Users/rcrooks/git-loc/social-ja
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated social-ja'
+cd /Users/rcrooks/git-loc/social-ko
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated social-ko'
+cd /Users/rcrooks/git-loc/social-zh-tw
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated social-zh-tw'
+# ssai
+cd /Users/rcrooks/git-loc/ssai-de
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated ssai-de'
+cd /Users/rcrooks/git-loc/ssai-es
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated ssai-es'
+cd /Users/rcrooks/git-loc/ssai-fr
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated ssai-fr'
+cd /Users/rcrooks/git-loc/ssai-ja
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated ssai-ja'
+cd /Users/rcrooks/git-loc/ssai-ko
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated ssai-ko'
+cd /Users/rcrooks/git-loc/ssai-zh-tw
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated ssai-zh-tw'
+# studio
+cd /Users/rcrooks/git-loc/studio-de
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated studio-de'
+cd /Users/rcrooks/git-loc/studio-es
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated studio-es'
+cd /Users/rcrooks/git-loc/studio-fr
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated studio-fr'
+cd /Users/rcrooks/git-loc/studio-ja
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated studio-ja'
+cd /Users/rcrooks/git-loc/studio-ko
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated studio-ko'
+cd /Users/rcrooks/git-loc/studio-zh-tw
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated studio-zh-tw'
+# zencoder
+cd /Users/rcrooks/git-loc/zencoder-de
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated zencoder-de'
+cd /Users/rcrooks/git-loc/zencoder-es
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated zencoder-es'
+cd /Users/rcrooks/git-loc/zencoder-fr
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated zencoder-fr'
+cd /Users/rcrooks/git-loc/zencoder-ja
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated zencoder-ja'
+cd /Users/rcrooks/git-loc/zencoder-ko
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated zencoder-ko'
+cd /Users/rcrooks/git-loc/zencoder-zh-tw
+git pull
+git add --all
+git commit --all -m general-update
+git push
+echo 'upated zencoder-zh-tw'
+
+echo 'all finished!'
