230defcef22bed0034489f1289999dc0918581ec
Bob Bailey <bbailey@brightcove.com>
2021-03-09 13:46:57 -0500

    remove index icon

--- _site/pull-all.sh
+++ _site/pull-all.sh
@@ -92,17 +92,9 @@ echo 'pulled studio'
 cd ../zencoder-support
 git pull
 
-# staging-site - temporarily beacon-la
-cd ../beacon-la
-git pull
-echo 'pulled staging site'
-# in-app help
-cd ../beacon-help
-git pull
-echo 'pulled staging site'
 
 # BCL-developer-docs
-cd ../BCL-developer-docs
+cd /Users/rcrooks/git-bcl/BCL-developer-docs
 git pull
 echo 'pulled zencoder-support'
 
